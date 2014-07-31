9750b6c3a7160268e7cc290b7ff1718c2808e4ad
Haleth <haleth@telenet.be>
2014-07-31 02:00:59 +0200

    Mess with some quality colour stuff.
    Not sure how well this will hold up, but it's a lot of unnecessary code
    to add quality colours for uncommon/common items otherwise.

--- FreeUI/scripts/globals.lua
+++ FreeUI/scripts/globals.lua
@@ -151,8 +151,12 @@ GOLD_AMOUNT = "%d\124TInterface\\MoneyFrame\\UI-GoldIcon:0:0:2:0\124t"
 
 -- [[ Item quality colours ]]
 
+-- Add quality colour for Poor items
+LE_ITEM_QUALITY_COMMON = -1
+BAG_ITEM_QUALITY_COLORS[LE_ITEM_QUALITY_POOR] = { r = 0.61, g = 0.61, b = 0.61}
 -- Change Common from grey to black
-BAG_ITEM_QUALITY_COLORS[LE_ITEM_QUALITY_COMMON] = { r= 0, g = 0, b = 0}
+BAG_ITEM_QUALITY_COLORS[LE_ITEM_QUALITY_COMMON] = { r = 0, g = 0, b = 0}
+BAG_ITEM_QUALITY_COLORS[1] = { r = 0, g = 0, b = 0}
 
 --[[ Chat mods ]]
 
