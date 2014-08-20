dcabddda4e07e4dba17db806df7476e50a8663d0
Haleth <haleth@telenet.be>
2014-08-20 16:58:18 +0200

    Improve LFG list frame.

--- FreeUI/theme/FrameXML/LFGList.lua
+++ FreeUI/theme/FrameXML/LFGList.lua
@@ -1,6 +1,8 @@
 local F, C = unpack(select(2, ...))
 
 tinsert(C.themes["FreeUI"], function()
+	local r, g, b = C.r, C.g, C.b
+
 	local LFGListFrame = LFGListFrame
 
 	-- [[ Category selection ]]
@@ -33,6 +35,123 @@ tinsert(C.themes["FreeUI"], function()
 		end
 	end)
 
+	-- [[ Search panel ]]
+
+	local SearchPanel = LFGListFrame.SearchPanel
+
+	SearchPanel.ResultsInset.Bg:Hide()
+	SearchPanel.ResultsInset:DisableDrawLayer("BORDER")
+
+	F.Reskin(SearchPanel.RefreshButton)
+	F.Reskin(SearchPanel.BackButton)
+	F.Reskin(SearchPanel.SignUpButton)
+	F.Reskin(SearchPanel.ScrollFrame.StartGroupButton)
+	F.ReskinInput(SearchPanel.SearchBox)
+	F.ReskinScroll(SearchPanel.ScrollFrame.scrollBar)
+
+	SearchPanel.RefreshButton:SetSize(24, 24)
+	SearchPanel.RefreshButton.Icon:SetPoint("CENTER")
+
+	-- Auto complete frame
+
+	SearchPanel.AutoCompleteFrame.BottomLeftBorder:Hide()
+	SearchPanel.AutoCompleteFrame.BottomRightBorder:Hide()
+	SearchPanel.AutoCompleteFrame.BottomBorder:Hide()
+	SearchPanel.AutoCompleteFrame.LeftBorder:Hide()
+	SearchPanel.AutoCompleteFrame.RightBorder:Hide()
+
+	local function resultOnEnter(self)
+		self.hl:Show()
+	end
+
+	local function resultOnLeave(self)
+		self.hl:Hide()
+	end
+
+	local numResults = 1
+	hooksecurefunc("LFGListSearchPanel_UpdateAutoComplete", function(self)
+		local AutoCompleteFrame = self.AutoCompleteFrame
+
+		for i = numResults, #AutoCompleteFrame.Results do
+			local result = AutoCompleteFrame.Results[i]
+
+			if numResults == 1 then
+				result:SetPoint("TOPLEFT", AutoCompleteFrame.LeftBorder, "TOPRIGHT", -8, 1)
+				result:SetPoint("TOPRIGHT", AutoCompleteFrame.RightBorder, "TOPLEFT", 5, 1)
+			else
+				result:SetPoint("TOPLEFT", AutoCompleteFrame.Results[i-1], "BOTTOMLEFT", 0, 1)
+				result:SetPoint("TOPRIGHT", AutoCompleteFrame.Results[i-1], "BOTTOMRIGHT", 0, 1)
+			end
+
+			result:SetNormalTexture("")
+			result:SetPushedTexture("")
+			result:SetHighlightTexture("")
+
+			local hl = result:CreateTexture(nil, "BACKGROUND")
+			hl:SetAllPoints()
+			hl:SetTexture(C.media.backdrop)
+			hl:SetVertexColor(r, g, b, .2)
+			hl:Hide()
+			result.hl = hl
+
+			F.CreateBD(result, .5)
+
+			result:HookScript("OnEnter", resultOnEnter)
+			result:HookScript("OnLeave", resultOnLeave)
+
+			numResults = numResults + 1
+		end
+	end)
+
+	-- [[ Application viewer ]]
+
+	local ApplicationViewer = LFGListFrame.ApplicationViewer
+
+	ApplicationViewer.InfoBackground:Hide()
+
+	ApplicationViewer.Inset.Bg:Hide()
+	ApplicationViewer.Inset:DisableDrawLayer("BORDER")
+
+	local function headerOnEnter(self)
+		self.hl:Show()
+	end
+
+	local function headerOnLeave(self)
+		self.hl:Hide()
+	end
+
+	for _, headerName in pairs({"NameColumnHeader", "RoleColumnHeader", "ItemLevelColumnHeader"}) do
+		local header = ApplicationViewer[headerName]
+		header.Left:Hide()
+		header.Middle:Hide()
+		header.Right:Hide()
+
+		header:SetHighlightTexture("")
+
+		local hl = header:CreateTexture(nil, "BACKGROUND")
+		hl:SetAllPoints()
+		hl:SetTexture(C.media.backdrop)
+		hl:SetVertexColor(r, g, b, .2)
+		hl:Hide()
+		header.hl = hl
+
+		F.CreateBD(header, .25)
+
+		header:HookScript("OnEnter", headerOnEnter)
+		header:HookScript("OnLeave", headerOnLeave)
+	end
+
+	ApplicationViewer.RoleColumnHeader:SetPoint("LEFT", ApplicationViewer.NameColumnHeader, "RIGHT", 1, 0)
+	ApplicationViewer.ItemLevelColumnHeader:SetPoint("LEFT", ApplicationViewer.RoleColumnHeader, "RIGHT", 1, 0)
+
+	F.Reskin(ApplicationViewer.RefreshButton)
+	F.Reskin(ApplicationViewer.RemoveEntryButton)
+	F.Reskin(ApplicationViewer.EditButton)
+	F.ReskinScroll(LFGListApplicationViewerScrollFrameScrollBar)
+
+	ApplicationViewer.RefreshButton:SetSize(24, 24)
+	ApplicationViewer.RefreshButton.Icon:SetPoint("CENTER")
+
 	-- [[ Entry creation ]]
 
 	local EntryCreation = LFGListFrame.EntryCreation
@@ -57,4 +176,22 @@ tinsert(C.themes["FreeUI"], function()
 	F.ReskinDropDown(EntryCreation.ActivityDropDown)
 	F.ReskinCheck(EntryCreation.ItemLevel.CheckButton)
 	F.ReskinCheck(EntryCreation.VoiceChat.CheckButton)
+
+	-- Activity finder
+
+	local ActivityFinder = EntryCreation.ActivityFinder
+
+	ActivityFinder.Background:SetTexture("")
+	ActivityFinder.Dialog.Bg:Hide()
+	for i = 1, 9 do
+		select(i, ActivityFinder.Dialog.BorderFrame:GetRegions()):Hide()
+	end
+
+	F.CreateBD(ActivityFinder.Dialog)
+	ActivityFinder.Dialog:SetBackdropColor(.2, .2, .2, .9)
+
+	F.Reskin(ActivityFinder.Dialog.SelectButton)
+	F.Reskin(ActivityFinder.Dialog.CancelButton)
+	F.ReskinInput(ActivityFinder.Dialog.EntryBox)
+	F.ReskinScroll(LFGListEntryCreationSearchScrollFrameScrollBar)
 end)
