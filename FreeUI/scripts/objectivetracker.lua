07fa1cedd7ff6696c60b37ff6bad8191d0dd79e2
Haleth <haleth@telenet.be>
2014-08-24 22:15:03 +0200

    Restyle new objective tracker (or try to).

--- FreeUI/scripts/objectivetracker.lua
+++ FreeUI/scripts/objectivetracker.lua
@@ -1,16 +1,19 @@
 local F, C, L = unpack(select(2, ...))
 
 local ot = ObjectiveTrackerFrame
+local BlocksFrame = ot.BlocksFrame
+
+-- [[ Positioning ]]
 
 local function moveTracker()
 	local xCoord, yAnchor
 
 	if MultiBarLeft:IsShown() then
-		xCoord = -85
+		xCoord = -84
 	elseif MultiBarRight:IsShown() then
-		xCoord = -55
+		xCoord = -57
 	else
-		xCoord = -17
+		xCoord = -30
 	end
 
 	yAnchor = VehicleSeatIndicator:IsShown() and VehicleSeatIndicator or Minimap
@@ -33,11 +36,18 @@ hooksecurefunc(VehicleSeatIndicator, "SetPoint", function(_, _, anchor)
 	end
 end)
 
+-- [[ Header ]]
+
+-- Header
+
+F.SetFS(ot.HeaderMenu.Title)
+
+-- Minimize button
+
 local minimizeButton = ot.HeaderMenu.MinimizeButton
 
 F.ReskinExpandOrCollapse(minimizeButton)
 minimizeButton:SetSize(15, 15)
-minimizeButton:SetPoint("TOPRIGHT", -12, 0)
 minimizeButton.plus:Hide()
 
 hooksecurefunc("ObjectiveTracker_Collapse", function()
@@ -47,57 +57,64 @@ hooksecurefunc("ObjectiveTracker_Expand", function()
 	minimizeButton.plus:Hide()
 end)
 
-F.SetFS(ot.BlocksFrame.QuestHeader.Text)
-F.SetFS(ot.BlocksFrame.AchievementHeader.Text)
-F.SetFS(ot.BlocksFrame.ScenarioHeader.Text)
+-- [[ Blocks and lines ]]
 
--- hooksecurefunc("ObjectiveTracker_AddBlock", function(block)
-	-- if not block.styled then
-		-- F.SetFS(block.HeaderText)
+DEFAULT_OBJECTIVE_TRACKER_MODULE.lineSpacing = 6
 
-		-- for _, line in block.lines do
-			-- F.SetFS(line.Text)
-			-- F.SetFS(line.Dext)
-			-- line.Text:SetSpacing(2)
-		-- end
+for _, headerName in pairs({"QuestHeader", "AchievementHeader", "ScenarioHeader"}) do
+	local header = BlocksFrame[headerName]
 
-		-- block.styled = true
-	-- end
--- end)
+	header.Background:Hide()
+	F.SetFS(header.Text)
+end
 
--- local index = 1
--- local itemIndex = 1
+hooksecurefunc("ObjectiveTracker_AddBlock", function(block)
+	local heightToSubstract = 0
+	local heightToAdd = 0
 
--- hooksecurefunc("ObjectiveTracker_Update", function()
-	-- local line = _G["WatchFrameLine"..index]
-	-- while line do
-		-- F.SetFS(line.text)
-		-- F.SetFS(line.dash)
-		-- line.text:SetSpacing(2)
+	if not block.styled then
+		heightToSubstract = block.HeaderText:GetHeight()
+		F.SetFS(block.HeaderText)
+		heightToAdd = block.HeaderText:GetHeight()
 
-		-- line.text:ClearAllPoints()
-		-- line.text:SetPoint("LEFT", line.dash, "RIGHT", -2, 3)
+		if block.lines then
+			for _, line in pairs(block.lines) do
+				if not line.styled then
+					heightToSubstract = heightToSubstract + line:GetHeight()
 
-		-- index = index + 1
-		-- line = _G["WatchFrameLine"..index]
-	-- end
+					F.SetFS(line.Text)
+					F.SetFS(line.Dash)
+					line.Text:SetSpacing(2)
 
-	-- local bu = _G["WatchFrameItem"..itemIndex]
-	-- while bu do
-		-- local hotkey = _G["WatchFrameItem"..itemIndex.."HotKey"]
+					line:SetHeight(line.Text:GetHeight())
 
-		-- bu:SetNormalTexture("")
-		-- bu:SetPushedTexture("")
-		-- F.CreateBG(bu)
+					heightToAdd = heightToAdd + line:GetHeight()
 
-		-- _G["WatchFrameItem"..itemIndex.."IconTexture"]:SetTexCoord(.08, .92, .08, .92)
+					line.styled = true
+				end
+			end
+		end
 
-		-- hotkey:ClearAllPoints()
-		-- hotkey:SetPoint("TOP", bu, -1, 0)
-		-- F.SetFS(hotkey)
-		-- hotkey:SetJustifyH("CENTER")
+		local itemButton = block.itemButton
 
-		-- itemIndex = itemIndex + 1
-		-- bu = _G["WatchFrameItem"..itemIndex]
-	-- end
--- end)
+		if itemButton and not itemButton.styled then
+			itemButton:SetNormalTexture("")
+			itemButton:SetPushedTexture("")
+
+			itemButton.HotKey:ClearAllPoints()
+			itemButton.HotKey:SetPoint("TOP", itemButton, -1, 0)
+			itemButton.HotKey:SetJustifyH("CENTER")
+			F.SetFS(itemButton.HotKey)
+
+			itemButton.icon:SetTexCoord(.08, .92, .08, .92)
+			F.CreateBG(itemButton)
+
+			itemButton.styled = true
+		end
+
+		block.styled = true
+	end
+
+	block.height = block.height - heightToSubstract + heightToAdd
+	block:SetHeight(block.height)
+end)
