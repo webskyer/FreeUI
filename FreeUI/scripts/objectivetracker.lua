f4e54454893fa27750560bcd53f10bf4f5209236
Haleth <haleth@telenet.be>
2014-09-05 03:33:16 +0200

    Objective tracker be damned.
    Should actually work properly now with no more random lines not being
    modified, item buttons being skipped or line spacing being messed up,
    but you never know.

--- FreeUI/scripts/objectivetracker.lua
+++ FreeUI/scripts/objectivetracker.lua
@@ -60,6 +60,7 @@ end)
 -- [[ Blocks and lines ]]
 
 DEFAULT_OBJECTIVE_TRACKER_MODULE.lineSpacing = 6
+DEFAULT_OBJECTIVE_TRACKER_MODULE.blockOffsetY = -11
 
 for _, headerName in pairs({"QuestHeader", "AchievementHeader", "ScenarioHeader"}) do
 	local header = BlocksFrame[headerName]
@@ -68,59 +69,66 @@ for _, headerName in pairs({"QuestHeader", "AchievementHeader", "ScenarioHeader"
 	F.SetFS(header.Text)
 end
 
+hooksecurefunc(DEFAULT_OBJECTIVE_TRACKER_MODULE, "SetBlockHeader", function(_, block)
+	if not block.headerStyled then
+		F.SetFS(block.HeaderText)
+		block.height = block.HeaderText:GetHeight()
+
+		block.headerStyled = true
+	end
+end)
+
+hooksecurefunc(QUEST_TRACKER_MODULE, "SetBlockHeader", function(_, block)
+	if not block.headerStyled then
+		F.SetFS(block.HeaderText)
+		block.height = block.HeaderText:GetHeight()
+
+		block.headerStyled = true
+	end
+
+	local itemButton = block.itemButton
+
+	if itemButton and not itemButton.styled then
+		itemButton:SetNormalTexture("")
+		itemButton:SetPushedTexture("")
+
+		itemButton.HotKey:ClearAllPoints()
+		itemButton.HotKey:SetPoint("TOP", itemButton, -1, 0)
+		itemButton.HotKey:SetJustifyH("CENTER")
+		F.SetFS(itemButton.HotKey)
+
+		itemButton.icon:SetTexCoord(.08, .92, .08, .92)
+		F.CreateBG(itemButton)
+
+		itemButton.styled = true
+	end
+end)
+
 hooksecurefunc("ObjectiveTracker_AddBlock", function(block)
-	-- to adjust for header and line text possibly taking up more or less lines
-	local heightToSubstract = 0
-	local heightToAdd = 5 -- default spacing between blocks is a bit small
+	-- to adjust for line text possibly taking up more or less lines
+	local heightDiff = 0
 
-	if not block.styled then
-		if block.HeaderText then
-			heightToSubstract = block.HeaderText:GetHeight()
-			F.SetFS(block.HeaderText)
-			heightToAdd = block.HeaderText:GetHeight()
-		end
+	if block.lines then
+		for _, line in pairs(block.lines) do
+			if not line.styled then
+				heightDiff = heightDiff - line:GetHeight()
 
-		if block.lines then
-			for _, line in pairs(block.lines) do
-				if not line.styled then
-					heightToSubstract = heightToSubstract + line:GetHeight()
+				F.SetFS(line.Text)
+				F.SetFS(line.Dash)
+				line.Text:SetSpacing(2)
 
-					F.SetFS(line.Text)
-					F.SetFS(line.Dash)
-					line.Text:SetSpacing(2)
+				line:SetHeight(line.Text:GetHeight())
 
-					line:SetHeight(line.Text:GetHeight())
+				heightDiff = heightDiff + line:GetHeight()
 
-					heightToAdd = heightToAdd + line:GetHeight()
-
-					line.styled = true
-				end
+				line.styled = true
 			end
 		end
-
-		local itemButton = block.itemButton
-
-		if itemButton and not itemButton.styled then
-			itemButton:SetNormalTexture("")
-			itemButton:SetPushedTexture("")
-
-			itemButton.HotKey:ClearAllPoints()
-			itemButton.HotKey:SetPoint("TOP", itemButton, -1, 0)
-			itemButton.HotKey:SetJustifyH("CENTER")
-			F.SetFS(itemButton.HotKey)
-
-			itemButton.icon:SetTexCoord(.08, .92, .08, .92)
-			F.CreateBG(itemButton)
-
-			itemButton.styled = true
-		end
-
-		block.styled = true
 	end
 
 	-- blocks without .HeaderText or .lines seem to always have .height = 0
-	if block.height ~= 0 then
-		block.height = block.height - heightToSubstract + heightToAdd
+	if block.height ~= 0 and heightDiff ~= 0 then
+		block.height = block.height + heightDiff
 		block:SetHeight(block.height)
 	end
 end)
